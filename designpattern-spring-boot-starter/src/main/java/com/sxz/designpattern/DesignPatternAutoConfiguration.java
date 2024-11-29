8b6c773d0f36f736f3eed88b3b8232ddade83d88
sxz <123456789@example.com>
2024-11-29 18:00:25 +0800

    框架构建

---
+++ designpattern-spring-boot-starter/src/main/java/com/sxz/designpattern/DesignPatternAutoConfiguration.java
@@ -0,0 +1,15 @@
+package com.sxz.designpattern;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description Policy mode automatic assembly class.
+ * @author: sxz
+ * @create: 2024-11-28 20:13
+ **/
+@Configuration
+public class DesignPatternAutoConfiguration {
+    public void test(){
+        System.out.println("自动装配测试成功！");
+    }
+}
