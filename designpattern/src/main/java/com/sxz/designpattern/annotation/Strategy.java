09bdbfd63c1025d744d0d6b7bd6318d0c12f2c25
sxz <123456789@example.com>
2024-12-11 19:46:22 +0800

    Design pattern first edition.

--- designpattern/src/main/java/com/sxz/designpattern/annotation/Strategy.java
+++ designpattern/src/main/java/com/sxz/designpattern/annotation/Strategy.java
@@ -1,15 +1,13 @@
 package com.sxz.designpattern.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * @author suxz
  */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface Strategy {
 
     /**
